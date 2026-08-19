08ab21936a40417fc9c9c0f3080fe18b4ab95689
Ox52 <harsh04241@gmail.com>
2026-08-19 10:11:50 +0530

    -

--- main.go
+++ main.go
@@ -5,14 +5,41 @@ import (
 	"net/http"
 )
 
+type Task struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var tasks = []Task{
+	{
+
+		ID:        1,
+		Title:     "learn go",
+		Completed: false,
+	},
+	{
+		ID:        2,
+		Title:     "Build API",
+		Completed: false,
+	},
+}
+
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		fmt.Println(w, "Task api is running ")
+		fmt.Fprintln(w, "Task api is running ")
 
 	})
+
+	http.HandleFunc("/tasks", getTask)
 	fmt.Println(" server is runnuing on http://localhost:8000")
 
 	http.ListenAndServe(":8000", nil)
+
+}
+func getTask(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, tasks)
 }
